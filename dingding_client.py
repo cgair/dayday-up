7c9d9fab98dc908b1a18bb7544752f9f4e1035e7
CGair23 <chengeair23@sjtu.edu.cn>
2022-07-22 16:52:58 +0800

    support at one person

--- dingding_client.py
+++ dingding_client.py
@@ -33,6 +33,29 @@ def send_msg(content_txt):
     response = request.getResponse()
     return response
 
- 
+
+def send_msg_at(content_txt):
+    timestamp = str(round(time.time() * 1000))
+    secret_enc = SECRET.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, SECRET)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    webhook = WEBHOOK.format(timestamp,sign)
+    request = dingtalk.api.OapiRobotSendRequest(webhook)
+    request.msgtype = 'text'
+    request.text = {
+        "content": content_txt
+    }
+    # atMobiles:钉钉群中所对应的成员手机号
+    # isAtAll：当设置为True时，发送消息时@所有人
+    request.at = {
+            "atMobiles":["15383469010"],
+            "isAtAll": False
+        }
+
+    response = request.getResponse()
+    return response
+
 # if __name__ == '__main__':
 #     send_msg()
