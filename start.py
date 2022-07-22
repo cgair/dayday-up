9300616a15c731701ac6ca17542c8d67bb50488e
CGair23 <chengeair23@sjtu.edu.cn>
2022-07-22 17:19:42 +0800

    ensure we can start job2

--- start.py
+++ start.py
@@ -40,6 +40,7 @@ def job_1(config: Config):
             
 
 def job_2(config: Config):
+    logging.debug("Starting job2...")
     config = config['need2']
     handler = []
     for value in config.values():
